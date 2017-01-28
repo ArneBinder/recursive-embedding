bf0a533a1559f22d5ee7ba843d46022f8215aeb5
Arne Binder <arne.b.binder@gmail.com>
2017-01-28 21:54:01 +0100

    first network stub

--- recursive_dependency_embeddings.py
+++ recursive_dependency_embeddings.py
@@ -5,6 +5,10 @@ from visualize import visualize
 import numpy as np
 import spacy
 import constants
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
 
 dim = 300
 edge_count = 60
@@ -47,7 +51,70 @@ for i, g in enumerate(graphs):
               data_embedding_maps_human, edge_map)
 
 
+class Net(nn.Module):
+    def __init__(self, data_vecs, edge_count, dim):
+        super(Net, self).__init__()
 
+        # dimension of embeddings
+        self.dim = dim
+
+        self.data_vecs = data_vecs
+        self.data_weights = {}
+        self.data_biases = {}
+        for data_type in self.data_vecs.keys():
+            vecs = data_vecs[data_type]
+            _, vec_dim = vecs.shape
+            self.data_weights[data_type] = Variable(torch.zeros(vec_dim, dim), requires_grad=True)
+            self.data_biases[data_type] = Variable(torch.zeros(dim), requires_grad=True)
+
+        self.edge_weights = {}
+        self.edge_biases = {}
+        for i in range(edge_count):
+            self.edge_weights[data_type] = Variable(torch.zeros(dim, dim), requires_grad=True)
+            self.edge_biases[data_type] = Variable(torch.zeros(dim), requires_grad=True)
+
+        self.score_embedding_weights = Variable(torch.zeros(1, dim), requires_grad=True)
+        self.score_embedding_biases = Variable(torch.zeros(dim), requires_grad=True)
+        self.score_data_weights = Variable(torch.zeros(1, dim), requires_grad=True)
+        self.score_data_biases = Variable(torch.zeros(dim), requires_grad=True)
+
+    def calc_embedding(self, data, types, parents, edges):
+
+        # connect roots
+        roots = [i for i, parent in enumerate(parents) if parent == 0]
+        for i in range(len(roots) - 1):
+            parents[roots[i]] = roots[i + 1]
+
+        # TODO: implement!
+
+        # dummy result
+        return Variable(torch.zeros(self.dim), requires_grad=True)
+
+    def calc_score(self, embedding, data_embedding):
+        return (self.score_embedding_biases + embedding) * self.score_embedding_weights \
+               + (self.score_data_biases + data_embedding) * self.score_data_weights
+
+    def forward(self, data, types, edges, graphs, pos):
+        data_vec = torch.from_numpy(data[pos])
+        data_embedding = data_vec * self.data_weights[types[pos]] + self.data_biases[types[pos]]
+        correct_edge = edges[pos]
+
+        embeddings = []
+        scores = []
+        for parents in graphs:
+            # calc embedding for correct graph at first
+            embedding = self.calc_embedding(data, types, parents, edges)
+            embeddings.append(embedding)
+            scores.append(self.calc_score(embedding, data_embedding))
+            for edge in range(edge_count):
+                if edge == correct_edge:    # already calculated
+                    continue
+                edges[pos] = edge
+                embedding = self.calc_embedding(data, types, parents, edges)
+                embeddings.append(embedding)
+                scores.append(self.calc_score(embedding, data_embedding))
+
+        # TODO: implement selection of best-scored embedding!
 
 
 
