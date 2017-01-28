6cab491d01cc6479afc1ca59c39d8bf79aa7126c
Arne Binder <arne.b.binder@gmail.com>
2017-01-28 19:04:36 +0100

    renaming

--- constants.py
+++ constants.py
@@ -1,2 +1,8 @@
 
+# data types
 WORD_EMBEDDING = 0
+
+
+# human readable labels
+NOT_IN_WORD_DICT = u'NOTINDICT'
+INTER_TREE = u'INTERTREE'
--- preprocessing.py
+++ preprocessing.py
@@ -3,14 +3,14 @@ import csv
 from sys import maxsize
 import numpy as np
 
-from tools import fn_timer
+from tools import fn_timer, list_powerset
 import constants
 
 
 @fn_timer
 def get_word_embeddings(vocab):
     vecs = np.ndarray(shape=(len(vocab)+1, vocab.vectors_length), dtype=np.float32)
-    m_human = [u'UNKNOWN']
+    m_human = [constants.NOT_IN_WORD_DICT]
     vecs[0] = np.zeros(vocab.vectors_length)
     m = {}
     i = 1
@@ -27,7 +27,7 @@ def process_sentence(sentence, parsed_data, max_forest_count, dep_map, data_embe
     # see read_data
     sen_data = list()
     sen_types = list()
-    sen_heads = list()
+    sen_parents = list()
     sen_edges = list()
     for i in range(sentence.start, sentence.end):
         # count roots (head points to self) and temp roots (head points to future token)
@@ -39,7 +39,7 @@ def process_sentence(sentence, parsed_data, max_forest_count, dep_map, data_embe
         data_type = constants.WORD_EMBEDDING
         sen_types.append(data_type)
         token = parsed_data[i]
-        sen_heads.append(token.head.i - i)
+        sen_parents.append(token.head.i - i)
         sen_edges.append(token.dep)
         try:
             x = data_embedding_maps[data_type][token.orth]
@@ -50,7 +50,7 @@ def process_sentence(sentence, parsed_data, max_forest_count, dep_map, data_embe
 
         # collect dependency labels for human readable mapping
         dep_map[token.dep] = token.dep_
-    return sen_data, sen_types, sen_heads, sen_edges
+    return sen_data, sen_types, sen_parents, sen_edges
 
 
 def dummy_str_reader():
@@ -87,7 +87,7 @@ def read_data(reader, nlp, data_embedding_maps, max_forest_count=10, max_sen_len
     # ids (dictionary) of relations to the heads (parents)
     seq_edges = list()
     # ids (sequence) of the heads (parents)
-    seq_heads = list()
+    seq_parents = list()
 
     offset = 0
     for parsed_data in nlp.pipe(reader(**args), n_threads=4, batch_size=1000):
@@ -104,14 +104,14 @@ def read_data(reader, nlp, data_embedding_maps, max_forest_count=10, max_sen_len
                 skipped_count += len(sentence)
                 continue
 
-            sen_data, sen_types, sen_heads, sen_edges = processed_sen
-            seq_heads += sen_heads
+            sen_data, sen_types, sen_parents, sen_edges = processed_sen
+            seq_parents += sen_parents
             seq_edges += sen_edges
             seq_data += sen_data
             seq_types += sen_types
 
             if prev_root is not None:
-                seq_heads[prev_root] = sentence.root.i - skipped_count + offset - prev_root
+                seq_parents[prev_root] = sentence.root.i - skipped_count + offset - prev_root
             prev_root = sentence.root.i - skipped_count + offset
         offset = len(seq_data)
 
@@ -121,8 +121,50 @@ def read_data(reader, nlp, data_embedding_maps, max_forest_count=10, max_sen_len
     for i in range(len(seq_edges)):
         seq_edges[i] = mapping[seq_edges[i]]
     dep_map = {mapping[key]: value for key, value in dep_map.iteritems()}
-    dep_map[0] = u'INTTREE'
+    dep_map[0] = constants.INTER_TREE
 
-    return (seq_data, seq_types, seq_heads, seq_edges), dep_map
+    return (seq_data, seq_types, seq_parents, seq_edges), dep_map
 
 
+# creates a list of possible graphs by trying to link the last data point indicated by ind to the graph of previous ones
+# result[0] contains the correct graph
+#   len(result) = (ind + 2) * r_c, r_c: count of roots (before ind)
+def graph_candidates(parents, ind):
+    assert ind < len(parents), 'ind = ' + str(ind) + ' exceeds data length = ' + str(len(parents))
+    sliced_parents = np.array(subgraph(parents, 0, ind + 1))
+    sliced_parents_predict = np.array(subgraph(parents, 0, ind))
+    roots = [i for i, parent in enumerate(sliced_parents_predict) if parent == 0]
+
+    sliced_parents_candidates = [sliced_parents]
+    for parent_candidate in range(ind+1):
+        candidate_temp = np.append(sliced_parents_predict, [parent_candidate - ind])
+        # find root of parent_candidate
+        parent_candidate_root = parent_candidate
+        while candidate_temp[parent_candidate_root] != 0:
+            parent_candidate_root = parent_candidate_root + candidate_temp[parent_candidate_root]
+        # temporarily remove the root whose tree contains the newly added element
+        if parent_candidate_root != ind:
+            roots.remove(parent_candidate_root)
+        for roots_subset in list_powerset(roots):
+            candidate = candidate_temp.copy()
+            for root in roots_subset:
+                candidate[root] = ind - root
+            if not np.array_equal(sliced_parents, candidate):
+                sliced_parents_candidates.append(candidate)
+
+        # re-add root
+        if parent_candidate_root != ind:
+            roots.append(parent_candidate_root)
+
+    return sliced_parents_candidates
+
+
+# creates a subgraph of the forest represented by parents
+# parents outside the new graph are linked to itself
+def subgraph(parents, start, end):
+    assert start < len(parents), 'start_ind = ' + str(start) + ' exceeds list size = ' + str(len(parents))
+    new_parents = parents[start:end]
+    for i in range(len(new_parents)):
+        if new_parents[i] < -i or new_parents[i] >= len(new_parents) - i:
+            new_parents[i] = 0
+    return new_parents
