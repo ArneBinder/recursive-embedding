d01ca2872234632d79085ffc57ede771eae14a1b
Arne Binder <arne.b.binder@gmail.com>
2017-01-29 01:58:30 +0100

    finished network stub

--- recursive_dependency_embeddings.py
+++ recursive_dependency_embeddings.py
@@ -9,9 +9,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
+import torch.optim as optim
 
 dim = 300
-edge_count = 60
+# edge_count = 60
 seq_length = 10
 
 slice_size = 75
@@ -36,11 +37,15 @@ data_dir = '/media/arne/DATA/DEVELOPING/ML/data/'
 
 
 class Net(nn.Module):
-    def __init__(self, data_vecs, edge_count, dim):
+    def __init__(self, data_vecs, edge_count, dim, slice_size, max_forest_count):
         super(Net, self).__init__()
 
         # dimension of embeddings
         self.dim = dim
+
+        self.slice_size = slice_size
+        self.max_forest_count = max_forest_count
+        self.max_graph_count = (slice_size + 1) * 2 ** (max_forest_count - 1)
 
         self.data_vecs = {}
         self.data_weights = {}
@@ -52,11 +57,11 @@ class Net(nn.Module):
             self.data_weights[data_type] = Variable(torch.zeros(vec_dim, dim), requires_grad=True)
             self.data_biases[data_type] = Variable(torch.zeros(dim), requires_grad=True)
 
-        self.edge_weights = {}
-        self.edge_biases = {}
-        for i in range(edge_count):
-            self.edge_weights[data_type] = Variable(torch.zeros(dim, dim), requires_grad=True)
-            self.edge_biases[data_type] = Variable(torch.zeros(dim), requires_grad=True)
+        self.edge_weights = Variable(torch.zeros(edge_count, dim, dim), requires_grad=True)
+        self.edge_biases = Variable(torch.zeros(edge_count, dim), requires_grad=True)
+        #for i in range(edge_count):
+        #    self.edge_weights[i] = torch.zeros(dim, dim) # Variable(torch.zeros(dim, dim), requires_grad=True)
+        #    self.edge_biases[i] = Variable(torch.zeros(dim), requires_grad=True)
 
         self.score_embedding_weights = Variable(torch.zeros(1, dim), requires_grad=True)
         self.score_embedding_biases = Variable(torch.zeros(dim), requires_grad=True)
@@ -100,19 +105,73 @@ class Net(nn.Module):
         data_embedding = data_vec * self.data_weights[types[pos]] + self.data_biases[types[pos]]
         correct_edge = edges[pos]
 
-        embeddings = []
-        scores = []
+        scores = Variable(torch.zeros(self.max_graph_count))
+        i = 0
         for parents in graphs:
             # calc embedding for correct graph at first
             embedding = self.calc_embedding(data, types, parents, edges)
-            embeddings.append(embedding)
-            scores.append(self.calc_score(embedding, data_embedding))
-            for edge in range(edge_count):
+            scores[i] = self.calc_score(embedding, data_embedding)
+            i += 1
+            for edge in range(self.edge_count):
                 if edge == correct_edge:    # already calculated
                     continue
                 edges[pos] = edge
                 embedding = self.calc_embedding(data, types, parents, edges)
-                embeddings.append(embedding)
-                scores.append(self.calc_score(embedding, data_embedding))
+                scores[i] = self.calc_score(embedding, data_embedding)
+                i += 1
 
-        # TODO: implement selection of best-scored embedding!
+        scores = nn.LogSoftmax().forward(scores)
+        # expected = torch.zeros(len(scores))
+        # expected[0] = 1
+        # criterion = nn.CrossEntropyLoss()
+        # loss = criterion(scores, expected)
+
+        return scores
+
+
+net = Net(data_vecs, len(edge_map_human), dim, slice_size, max_forest_count)
+
+params = list(net.parameters())
+print(len(params))
+
+criterion = nn.CrossEntropyLoss() # use a Classification Cross-Entropy loss
+optimizer = optim.Adagrad(net.parameters(), lr=0.01, lr_decay=0, weight_decay=0)    # default meta parameters
+
+expected = Variable(torch.zeros(slice_size))
+expected[0] = 1
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    slice_start = 0
+    #for i, data in enumerate(trainloader, 0):
+    while slice_start < len(seq_data):
+        for i in range(slice_start, min(len(seq_data), slice_start + slice_size)):
+            # get the inputs
+            data = seq_data[slice_start:i]
+            types = seq_types[slice_start:i]
+            parents = subgraph(seq_parents, slice_start, i)
+            edges = seq_edges[slice_start:i]
+
+            #inputs, labels = data
+
+            # wrap them in Variable
+            #inputs, labels = Variable(inputs), Variable(labels)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(Variable(data), Variable(types), Variable(parents), Variable(edges))
+            loss = criterion(outputs, expected)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.data[0]
+            if i % 100 == 99:  # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+        slice_start += slice_size
+print('Finished Training')
