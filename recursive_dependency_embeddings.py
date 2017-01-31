e2d9742374a5cbd68947fd6e2e80526a7a1a9805
Arne Binder <arne.b.binder@gmail.com>
2017-01-31 14:57:39 +0100

    #2

--- recursive_dependency_embeddings.py
+++ recursive_dependency_embeddings.py
@@ -12,12 +12,13 @@ from torch.autograd import Variable
 from net import Net
 from tools import mkdir_p
 from tensorboard_logger import configure, log_value
+import random
 
 dim = 300
 # edge_count = 60
 # seq_length = 10
 
-slice_size = 20  # 75
+max_slice_size = 20  # 75
 max_forest_count = 5  # 10
 
 nlp = spacy.load('en')
@@ -40,12 +41,12 @@ configure(log_dir, flush_secs=1)
 # create data arrays
 (seq_data, seq_types, seq_parents, seq_edges), edge_map_human = \
     read_data(articles_from_csv_reader, nlp, data_embedding_maps, max_forest_count=max_forest_count,
-              max_sen_length=slice_size,
+              max_sen_length=max_slice_size,
               args={'max_articles': 10, 'filename': data_dir + 'corpora/documents_utf8_filtered_20pageviews.csv'})
 
 print('data length:', len(seq_data))
 
-net = Net(data_vecs, len(edge_map_human), dim, slice_size, max_forest_count)
+net = Net(data_vecs, len(edge_map_human), dim, max_slice_size, max_forest_count)
 #loss_fn = torch.nn.L1Loss(size_average=True)
 loss_fn = torch.nn.CrossEntropyLoss(size_average=True)
 
@@ -55,33 +56,43 @@ print('variables to train:', len(params))
 # criterion = nn.CrossEntropyLoss() # use a Classification Cross-Entropy loss
 optimizer = optim.Adagrad(net.get_parameters(), lr=0.01, lr_decay=0, weight_decay=0)  # default meta parameters
 
-print('slice_size:', slice_size)
+epochs = 10
+
+print('max_slice_size:', max_slice_size)
 print('max_forest_count:', max_forest_count)
-print('max_graph_count: ', net.max_graph_count)
-print('edge_count: ', net.edge_count)
+print('max_graph_count:', net.max_graph_count)
+print('edge_count:', net.edge_count)
+print('epochs:', epochs)
 
 interval_avg = 50
-max_steps = 20  # len(seq_data)
+max_steps = 100  # per slice_size
 
-for epoch in range(50):
-    running_loss = 0.0
-    slice_start = 0
-    while slice_start < max_steps:
-        for i in range(slice_start + 1, min(max_steps, len(seq_data) + 1, slice_start + slice_size + 1)):
-            # get the inputs
-            data = np.array(seq_data[slice_start:i])
-            types = np.array(seq_types[slice_start:i])
-            parents = subgraph(seq_parents, slice_start, i)
-            edges = np.array(seq_edges[slice_start:i])
+for slice_size in range(1, max_slice_size):
+    for epoch in range(epochs):
+        running_loss = 0.0
+        slice_start = 0
+        # TODO: check loop end!
+        i = 0
+        slice_starts = range(0, min(max_steps*slice_size, len(seq_data)-slice_size), slice_size)
+        random.shuffle(slice_starts)
+        for slice_start in slice_starts:
+        #while slice_start < min(max_steps*slice_size, len(seq_data)-slice_size):
+            #for i in range(slice_start + 1, min(max_steps, len(seq_data) + 1, slice_start + max_slice_size + 1)):
+                # get the inputs
+            slice_end = slice_start + slice_size
+            data = np.array(seq_data[slice_start:slice_end])
+            types = np.array(seq_types[slice_start:slice_end])
+            parents = subgraph(seq_parents, slice_start, slice_end)
+            edges = np.array(seq_edges[slice_start:slice_end])
             if len([True for parent in parents if parent == 0]) > net.max_forest_count:
                 continue
-            graphs = np.array(graph_candidates(parents, i - slice_start - 1))
+            graphs = np.array(graph_candidates(parents, len(parents)-1))
             correct_edge = edges[-1]
             # zero the parameter gradients
             optimizer.zero_grad()
 
             # forward + backward + optimize
-            outputs = net(data, types, graphs, edges, i - slice_start - 1)
+            outputs = net(data, types, graphs, edges, len(parents)-1)
             outputs_cat = torch.cat(outputs).squeeze()
             expected = Variable(torch.cat(
                 (torch.zeros(correct_edge), torch.ones(1), torch.zeros(len(outputs_cat) - correct_edge - 1)))).type(
@@ -101,14 +112,19 @@ for epoch in range(50):
             #    average_loss = average_loss * interval_avg / num_steps
             # if i % step_size == step_size*10 -1:  # print every 2000 mini-batches
             # print('[%5d] loss: %.3f' % (i + 1, running_loss * interval_avg / num_steps))
-            print('[%d, %5d] loss: %15.3f   size: %2d' % (epoch + 1, i, running_loss, i - slice_start))
-            log_value('loss', running_loss, i)
-            running_loss = 0.0
 
-        slice_start += slice_size
-    model_fn = log_dir + 'model-' + '{:03d}'.format(epoch)
-    #print('write model to ' + model_fn)
-    #with open(model_fn, 'w') as f:
-    #    torch.save(net, f)
+            #print('[%d, %5d] loss: %15.3f   size: %2d' % (epoch + 1, i, running_loss, slice_size))
+            #log_value('loss', running_loss, i)
+
+
+            #slice_start += slice_size
+            i += 1
+
+        print('[%4d] loss: %15.3f   size: %2d' % (epoch + 1, running_loss / len(slice_starts), slice_size))
+
+        model_fn = log_dir + 'model-' + '{:03d}'.format(epoch)
+        #print('write model to ' + model_fn)
+        #with open(model_fn, 'w') as f:
+        #    torch.save(net, f)
 
 print('Finished Training')
