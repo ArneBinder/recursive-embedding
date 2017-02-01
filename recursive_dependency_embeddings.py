06255a7b0c52cb8d2b3c2a329ab0658d8f872f12
Arne Binder <arne.b.binder@gmail.com>
2017-02-01 14:04:53 +0100

    change hyper parameter

--- recursive_dependency_embeddings.py
+++ recursive_dependency_embeddings.py
@@ -35,7 +35,7 @@ def main():
     arg_parser.add_argument('-st', '--max-steps-per-epoch', type=int, default=1000)
     # increase slice_size if the skew over the last loss-history-size losses is smaller than loss-skew-threshold
     arg_parser.add_argument('-lh', '--loss-history-size', type=int, default=10)
-    arg_parser.add_argument('-sk', '--loss-skew-threshold', type=float, default=0.01)
+    arg_parser.add_argument('-sk', '--loss-skew-threshold', type=float, default=0.1)
     args = arg_parser.parse_args()
 
     print('ARGUMENTS:')
