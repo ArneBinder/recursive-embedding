0bfbda51e83b062bcd4bf8153d1991f194c19966
Arne Binder <arne.b.binder@gmail.com>
2017-05-15 21:10:17 +0200

    added process_sentence7: words, edges, entity type (if !=0), lemma (if different), pos-tag and process_sentence8: words, word type, edges, edge type, entity type (if !=0), entity type type, lemma (if different), lemma type, pos-tag, pos-tag type

--- preprocessing.py
+++ preprocessing.py
@@ -209,6 +209,151 @@ def process_sentence6(sentence, parsed_data, data_maps, dict_unknown=None):
     return result_data, result_parents, root_offset
 
 
+# embeddings for:
+# words, edges, entity type (if !=0),
+# lemma (if different), pos-tag
+def process_sentence7(sentence, parsed_data, data_maps, dict_unknown=None):
+    sen_data = list()
+    sen_parents = list()
+    sen_a = list()
+    sen_offsets = list()
+
+    last_offset = 0
+    for i in range(sentence.start, sentence.end):
+        token = parsed_data[i]
+        parent_offset = token.head.i - i
+        # add word embedding
+        sen_data.append(getOrAdd(data_maps, token.orth, dict_unknown))
+        sen_parents.append(parent_offset)
+        # additional data for this token
+        a_data = list()
+        a_parents = list()
+
+        # add edge type embedding
+        a_data.append(getOrAdd(data_maps, token.dep, dict_unknown))
+        a_parents.append(-len(a_data))
+        # add pos-tag type embedding
+        a_data.append(getOrAdd(data_maps, token.tag, dict_unknown))
+        a_parents.append(-len(a_data))
+
+        # add entity type embedding
+        if token.ent_type != 0:
+            a_data.append(getOrAdd(data_maps, token.ent_type, dict_unknown))
+            a_parents.append(-len(a_data))
+        # add lemma type embedding
+        if token.lemma != token.orth:
+            a_data.append(getOrAdd(data_maps, token.lemma, dict_unknown))
+            a_parents.append(-len(a_data))
+        sen_a.append((a_data, a_parents))
+        # count additional data for every main data point
+        current_offset = last_offset + len(a_data)
+        sen_offsets.append(current_offset)
+        last_offset = current_offset
+
+    root_offset = 0
+    result_data = list()
+    result_parents = list()
+    l = len(sen_data)
+    for i in range(l):
+        # set root
+        if sen_parents[i] == 0:
+            root_offset = len(result_data)
+        # add main data
+        result_data.append(sen_data[i])
+        # shift parent indices
+        parent_idx = sen_parents[i] + i
+        shift = tools.get_default(sen_offsets, parent_idx - 1, 0) - tools.get_default(sen_offsets, i - 1, 0)
+        # add (shifted) main parent
+        result_parents.append(sen_parents[i] + shift)
+        # insert additional data
+        a_data, a_parents = sen_a[i]
+        if len(a_data) > 0:
+            result_data.extend(a_data)
+            result_parents.extend(a_parents)
+
+    return result_data, result_parents, root_offset
+
+
+# embeddings for:
+# words, word type, edges, edge type, entity type (if !=0), entity type type,
+# lemma (if different), lemma type, pos-tag, pos-tag type
+def process_sentence8(sentence, parsed_data, data_maps, dict_unknown=None):
+    sen_data = list()
+    sen_parents = list()
+    sen_a = list()
+    sen_offsets = list()
+
+    last_offset = 0
+    for i in range(sentence.start, sentence.end):
+        token = parsed_data[i]
+        parent_offset = token.head.i - i
+        # add word embedding
+        sen_data.append(getOrAdd(data_maps, token.orth, dict_unknown))
+        sen_parents.append(parent_offset)
+        # additional data for this token
+        a_data = list()
+        a_parents = list()
+
+        # add word type type embedding
+        a_data.append(getOrAdd(data_maps, constants.WORD_EMBEDDING, dict_unknown))
+        a_parents.append(-len(a_data))
+        # add edge type embedding
+        a_data.append(getOrAdd(data_maps, token.dep, dict_unknown))
+        a_parents.append(-len(a_data))
+        # add edge type type embedding
+        a_data.append(getOrAdd(data_maps, constants.EDGE_EMBEDDING, dict_unknown))
+        a_parents.append(-1)
+        # add pos-tag type embedding
+        a_data.append(getOrAdd(data_maps, token.tag, dict_unknown))
+        a_parents.append(-len(a_data))
+        # add pos-tag type type embedding
+        a_data.append(getOrAdd(data_maps, constants.POS_TAG_EMBEDDING, dict_unknown))
+        a_parents.append(-1)
+
+        # add entity type embedding
+        if token.ent_type != 0:
+            a_data.append(getOrAdd(data_maps, token.ent_type, dict_unknown))
+            a_parents.append(-len(a_data))
+            # add entity type type embedding
+            a_data.append(getOrAdd(data_maps, constants.ENTITY_TYPE_EMBEDDING, dict_unknown))
+            a_parents.append(-1)
+        # add lemma type embedding
+        if token.lemma != token.orth:
+            a_data.append(getOrAdd(data_maps, token.lemma, dict_unknown))
+            a_parents.append(-len(a_data))
+            # add lemma type type embedding
+            a_data.append(getOrAdd(data_maps, constants.LEMMA_EMBEDDING, dict_unknown))
+            a_parents.append(-1)
+        sen_a.append((a_data, a_parents))
+        # count additional data for every main data point
+        current_offset = last_offset + len(a_data)
+        sen_offsets.append(current_offset)
+        last_offset = current_offset
+
+    root_offset = 0
+    result_data = list()
+    result_parents = list()
+    l = len(sen_data)
+    for i in range(l):
+        # set root
+        if sen_parents[i] == 0:
+            root_offset = len(result_data)
+        # add main data
+        result_data.append(sen_data[i])
+        # shift parent indices
+        parent_idx = sen_parents[i] + i
+        shift = tools.get_default(sen_offsets, parent_idx - 1, 0) - tools.get_default(sen_offsets, i - 1, 0)
+        # add (shifted) main parent
+        result_parents.append(sen_parents[i] + shift)
+        # insert additional data
+        a_data, a_parents = sen_a[i]
+        if len(a_data) > 0:
+            result_data.extend(a_data)
+            result_parents.extend(a_parents)
+
+    return result_data, result_parents, root_offset
+
+
 def dummy_str_reader():
     yield u'I like RTRC!'
 
