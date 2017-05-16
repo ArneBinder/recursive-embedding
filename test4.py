a4ddb61f10d71eaafde7998f188035270c9eed49
Arne Binder <arne.b.binder@gmail.com>
2017-05-17 01:58:33 +0200

    minor changes

--- test4.py
+++ test4.py
@@ -4,6 +4,8 @@ import preprocessing
 import corpus_wikipedia
 import numpy as np
 import pprint
+import tensorflow_fold as td
+import os
 
 
 seq_data = [3646, 117, 112, 385, 853, 120, 109, 2014, 7453, 70, 110, 145, 139, 70]
@@ -29,9 +31,15 @@ def test_build_build_sequence_tree_with_candidates():
     insert_idx = 5
     candidate_indices = [2, 8]
     seq_tree_c = preprocessing.build_sequence_tree_with_candidates(seq_data, seq_parents, children, roots[0], insert_idx, candidate_indices)
-    pp.pprint(seq_tree_c)
+    #pp.pprint(seq_tree_c)
+    pp.pprint(td.proto_tools.serialized_message_to_tree('recursive_dependency_embedding.SequenceNodeCandidates', seq_tree_c.SerializeToString()))
 
 if __name__ == '__main__':
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    td.proto_tools.map_proto_source_tree_path('', ROOT_DIR)
+    td.proto_tools.import_proto_file('sequence_node.proto')
+    td.proto_tools.import_proto_file('sequence_node_sequence.proto')
+    td.proto_tools.import_proto_file('sequence_node_candidates.proto')
     #test_depth()
     test_build_build_sequence_tree_with_candidates()
 
