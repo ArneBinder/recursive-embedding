43854ac9c65ee1b59185c63150c217e79092267a
Arne Binder <arne.b.binder@gmail.com>
2017-11-01 15:46:43 +0100

    use original files (cols split by '|||')

--- corpus_annoppdb.py
+++ corpus_annoppdb.py
@@ -10,33 +10,32 @@ tf.flags.DEFINE_string('corpus_name',
 
 FLAGS = tf.flags.FLAGS
 
-delimiter = '\t'
+delimiter = '|||'
 
 
 def sentence_reader(filename):
-    with open(filename, 'rb') as csvfile:
-        reader = csv.reader(csvfile, delimiter=delimiter, quotechar='"')
-        for row in reader:
-            if len(row) > 2:
-                yield row[0].decode('utf-8')
-                yield row[1].decode('utf-8')
+    with open(filename, 'rb') as file:
+        for line in file:
+            cols = line.split(delimiter)
+            if len(cols) > 2:
+                yield cols[0].decode('utf-8')
+                yield cols[1].decode('utf-8')
             else:
-                print('ERROR could not read line:')
-                print(row)
+                print(cols)
 
 
 def score_reader(filename):
-    with open(filename, 'rb') as csvfile:
-        reader = csv.reader(csvfile, delimiter=delimiter, quotechar='"')
-        for row in reader:
-            yield (float(row[2]) - 1.0) / 4.0
+    with open(filename, 'rb') as file:
+        for line in file:
+            cols = line.split(delimiter)
+            yield (float(cols[2].decode('utf-8')) - 1.0) / 4.0
 
 
 def main(args=None):
     if len(args) > 1:
         file_names = args[1:]
     else:
-        file_names = ['ppdb_all_fixed.txt', 'ppdb_dev_fixed.txt', 'ppdb_test_fixed.txt']
+        file_names = ['ppdb_train.txt', 'ppdb_dev.txt', 'ppdb_test.txt']
     corpus_simtuple.create_corpus(reader_sentences=sentence_reader, reader_score=score_reader,
                                   corpus_name=FLAGS.corpus_name,
                                   file_names=file_names)
