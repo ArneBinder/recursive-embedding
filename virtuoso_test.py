f2b5d6e49dd2197abc7143a6ce389fd0f0cbfd46
Arne Binder <arne.b.binder@gmail.com>
2018-01-31 23:50:05 +0100

    cleaned prerequisites

--- virtuoso_test.py
+++ virtuoso_test.py
@@ -6,44 +6,43 @@ from rdflib import Namespace
 from rdflib.namespace import RDF
 
 """
-uses https://github.com/maparent/virtuoso-python
-
-requires:
-    install: 
-        libvirtodbc0 (high-performance database - ODBC libraries (driver)) included in:
-            libvirtodbc0_7.2_amd64.deb (https://github.com/Dockerizing/triplestore-virtuoso7)
+prerequisites:
+    set up / install:
+        virtuoso docker image: 
+            see https://joernhees.de/blog/2015/11/23/setting-up-a-linked-data-mirror-from-rdf-dumps-dbpedia-2015-04-freebase-wikidata-linkedgeodata-with-virtuoso-7-2-1-and-docker-optional/
+        virtuoso dboc driver (libvirtodbc0) included in libvirtodbc0_7.2_amd64.deb (e.g. from https://github.com/Dockerizing/triplestore-virtuoso7)
+            wget https://github.com/Dockerizing/triplestore-virtuoso7/raw/master/libvirtodbc0_7.2_amd64.deb
+            sudo apt install /PATH/TO/libvirtodbc0_7.2_amd64.deb
+        rdflib:
+            pip install rdflib
+        virtuoso-python: see https://github.com/maparent/virtuoso-python
+            git clone git@github.com:maparent/virtuoso-python.git && cd virtuoso-python
+            pip install -r requirements.txt
+            python setup.py install
     
     settings:            
         ~/.odbc.ini (or /etc/odbc.ini) containing:
         "
-        [ODBC Data Sources]
-        VOS = Virtuoso
-        
         [VOS]
         Description = Open Virtuoso
-        Driver      = /usr/local/virtuoso-opensource/lib/virtodbcu_r.so
+        Driver      = /usr/lib/odbc/virtodbc_r.so
         Address     = localhost:1111
         Locale      = en.UTF-8
         "
-        
-        and ~/.odbcinst.ini (or /etc/odbcinst.ini) containing:
-        "
-        [ODBC Drivers]
-        Virtuoso = Installed
-        
-        [Virtuoso]
-        Driver = /usr/lib/odbc/virtodbc_r.so
-        Setup  = /usr/lib/odbc/virtodbc_r.so
+    
+    start virtuoso docker image (use 8gb of ram):
+        docker run -it -p 8890:8890 -p 1111:1111 -v ~/virtuoso_db:/var/lib/virtuoso-opensource-7 -e "NumberOfBuffers=$((8*85000))" joernhees/virtuoso
 """
 
 if __name__ == '__main__':
     print('set up connection ...')
     Virtuoso = plugin("Virtuoso", Store)
     store = Virtuoso("DSN=VOS;UID=dba;PWD=dba;WideAsUTF16=Y")
-    print('connected')
     default_graph_uri = "http://dbpedia.org/nif"
     g = Graph(store, identifier=URIRef(default_graph_uri))
+    print('connected')
     nif = Namespace("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#")
+    dbr = Namespace("http://dbpedia.org/resource/")
 
     for i, (nif_context, _, _) in enumerate(g.triples((None, RDF.type, nif.Context))):
         if i > 10:
