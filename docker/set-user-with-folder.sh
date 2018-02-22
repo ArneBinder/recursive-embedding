0f23d42524bb1ed395cdc07b3c9dd5846b624baa
Arne Binder <arne.b.binder@gmail.com>
2018-02-22 15:14:47 +0100

    removed echos

--- docker/set-user-with-folder.sh
+++ docker/set-user-with-folder.sh
@@ -7,33 +7,28 @@ WHO=$1
 shift
 
 stat $WHO > /dev/null || (echo You must mount a file to "$WHO" in order to properly assume user && exit 1)
-echo "out_path=$WHO"
+#echo "out_path=$WHO"
 
-echo "args:"
-echo "$@"
+#echo "args:"
+#echo "$@"
 
 USERID=$(stat -c %u $WHO)
 GROUPID=$(stat -c %g $WHO)
 
-echo "userid=$USERID"
-echo "groupid=$GROUPID"
+#echo "userid=$USERID"
+#echo "groupid=$GROUPID"
 
-
-#groupadd -g $GROUPID docker_user
-#id -u somegroupname &>/dev/null
 GROUPIDROOT=$(id -g root)
 echo "GROUPIDROOT=$GROUPIDROOT"
 if [ "$GROUPIDROOT" -ne "$GROUPID" ]; then
     deluser docker_user > /dev/null 2>&1
-    echo "deleted docker_user"
+    #echo "deleted docker_user"
     addgroup --gid $GROUPID docker_user
-    echo "added group docker_user"
-    #useradd -u $USERID -G docker_user -D -s /bin/sh docker_user
+    #echo "added group docker_user"
     useradd --uid $USERID --gid $GROUPID docker_user
-    echo "added user docker_user"
+    echo "execute as user=docker_user with uid=$USERID and gid=$GROUPID"
     gosu docker_user "$@"
 else
-    #echo "could not add group docker_user with groupid=$GROUPID, assume to be root"
     echo "user group is root, execute as root"
     gosu root "$@"
 fi
