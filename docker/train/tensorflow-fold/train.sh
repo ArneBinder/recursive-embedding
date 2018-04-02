462f9f9dba09c8d8c6280c0865426c169b1feb7c
Arne Binder <arne.b.binder@gmail.com>
2018-04-02 19:03:02 +0200

    optimized train script and added entrypoints to docker files

--- docker/train/tensorflow-fold/train.sh
+++ docker/train/tensorflow-fold/train.sh
@@ -4,7 +4,8 @@
 my_dir="$(dirname "$0")"
 source "$my_dir/.env"
 
-array=( TRAIN_DATA TRAIN_LOGDIR HOST_PORT_NOTEBOOK HOST_PORT_TENSORBOARD LIMIT_CPUS CPU_SET NV_GPU )
+## check variables content
+array=( HOST_CORPORA_OUT HOST_TRAIN TRAIN_DATA TRAIN_LOGDIR HOST_PORT_NOTEBOOK HOST_PORT_TENSORBOARD LIMIT_CPUS CPU_SET NV_GPU )
 echo ".env vars:"
 for i in "${array[@]}"
 do
@@ -16,21 +17,22 @@ do
 done
 
 
+## set command and docker image depending on if GPUs are configured as available
 if [ -n "$NV_GPU" ]; then
     COMMAND="NV_GPU=$NV_GPU nvidia-docker"
+    IMAGE="tensorflowfold:tf1_3_gpu"
 else
-    COMMAND=docker
+    COMMAND="docker"
+    IMAGE="tensorflowfold:tf1_3_cpu_mkl"
 fi
 
-echo execute command: $COMMAND
+echo "execute COMMAND: $COMMAND @IMAGE: $IMAGE"
 
 $COMMAND run \
     -v $HOST_TRAIN:/root/train \
     -v $HOST_CORPORA_OUT:/root/corpora_out \
     -p $HOST_PORT_NOTEBOOK:8888 \
-    tensorflowfold:tf1_3_cpu_mkl \
-    /root/recursive-embedding/set-user-with-folder.sh /root/train \
-    python train_fold.py \
+    $IMAGE \
         --train_data_path=/root/corpora_out/$TRAIN_DATA \
         --logdir=/root/train/$TRAIN_LOGDIR \
         --model_type=tuple \
