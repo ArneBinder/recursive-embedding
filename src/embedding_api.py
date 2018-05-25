832a7cc19875c992cdab3fa6a6c3e9d19b3d88c9
Arne Binder <arne.b.binder@gmail.com>
2018-05-25 17:15:26 +0200

    embedding_api tuple_scores: set root_id_str, if not present

--- src/embedding_api.py
+++ src/embedding_api.py
@@ -802,11 +802,12 @@ def get_tuple_scores():
         root_id_str = params.get('root_id_string', None)
         if root_id_str is not None:
             try:
-                root_id = forest.lexicon_roots.get_d(root_id_str, data_as_hashes=False)
+                params['root_id'] = forest.lexicon_roots.get_d(root_id_str, data_as_hashes=False)
             except KeyError:
                 raise InvalidUsage("root_id_string not found: %s" % root_id_str)
-        else:
-            root_id = params['root_id']
+        root_id = params['root_id']
+        if root_id_str is None:
+            params['root_id_string'] = forest.lexicon_roots.get_s(root_id, data_as_hashes=False)
         all_root_ids = current_root_ids()
         root_ids_target_strings = params.get('root_ids_target_string', None)
         if root_ids_target_strings is not None:
