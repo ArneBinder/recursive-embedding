c9e5de2649ee16091d8e04ec387dbe4b2d502d0f
Arne Binder <arne.b.binder@gmail.com>
2018-07-09 11:47:41 +0200

    re-enabled cache

--- src/train_fold.py
+++ src/train_fold.py
@@ -1332,7 +1332,7 @@ if __name__ == '__main__':
                 csvfile.flush()
     else:
         config = Config(logdir_continue=FLAGS.logdir_continue, logdir_pretrained=FLAGS.logdir_pretrained)
-        USE_CACHE = False
+        USE_CACHE = True
         # get default config (or load from logdir_continue/logdir_pretrained)
         config.init_flags()
         # pylint: disable=protected-access
