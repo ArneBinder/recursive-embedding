f37da51c03a76a22111ba202663c74cb926fa866
Arne Binder <arne.b.binder@gmail.com>
2018-07-26 12:20:28 +0200

    added f1 scores to parameter settings

--- docker/train/tensorflow-fold/parameters_all.jl
+++ docker/train/tensorflow-fold/parameters_all.jl
@@ -1,8 +1,10 @@
-# TREE
+# best parameters settings so far
+
+# TREE (F1: 0.63)
 {"tree_embedder": "TreeEmbedding_HTU_reduceSUM_mapGRU", "learning_rate": 0.001, "batch_size": 100, "fc_sizes": "2000", "concat_mode": "tree", "state_size": 900, "leaf_fc_size": 0}
-# TFIDF
+# TFIDF (F1: 0.64)
 {"tree_embedder": "tfidf", "learning_rate": 0.0001, "batch_size": 100, "fc_sizes": "2000", "concat_mode": "aggregate"}
-# BIGRU
+# BIGRU (F1: 0.50)
 {"tree_embedder": "TreeEmbedding_FLATconcat_BIGRU", "learning_rate": 0.0003, "batch_size": 25, "fc_sizes": "1000", "concat_mode": "aggregate", "state_size": 300, "leaf_fc_size": 0}
-# SUM
-{"tree_embedder": "TreeEmbedding_FLAT_SUM", "learning_rate": 0.001, "batch_size": 100, "fc_sizes": "1000", "concat_mode": "aggregate", "leaf_fc_size": 300}
+# SUM (F1: 0.60)
+{"tree_embedder": "TreeEmbedding_FLAT_SUM", "learning_rate": 0.0001, "batch_size": 100, "fc_sizes": "1000", "concat_mode": "aggregate", "leaf_fc_size": 600}
