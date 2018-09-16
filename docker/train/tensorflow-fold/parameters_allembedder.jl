986d6ab714945f15ba14b4c546e19fd8b6e99c7f
Arne Binder <arne.b.binder@gmail.com>
2018-09-17 00:58:38 +0200

    updated parameters file

--- docker/train/tensorflow-fold/parameters_allembedder.jl
+++ docker/train/tensorflow-fold/parameters_allembedder.jl
@@ -5,8 +5,8 @@
 # TFIDF (F1: 0.64)
 {"tree_embedder": "tfidf", "concat_mode": "aggregate", "learning_rate": 0.0001, "batch_size": 100}
 # GRU
-{"tree_embedder": "FLATconcat_GRU", "concat_mode": "aggregate", "leaf_fc_size": 0, "learning_rate": 0.001, "batch_size": 100}
+{"tree_embedder": "FLATconcat_GRU", "concat_mode": "aggregate", "leaf_fc_size": 0, "learning_rate": 0.0003, "batch_size": 100}
 # SUM (F1: 0.60)
 {"tree_embedder": "FLAT_SUM", "concat_mode": "aggregate", "learning_rate": 0.0001, "batch_size": 100}
 # TREE + TFIDF
-{"tree_embedder": "HTU_reduceSUM_mapGRU", "concat_mode": "tree", "use_tfidf": true, "leaf_fc_size": 0, "learning_rate": 0.001, "batch_size": 100}
+{"tree_embedder": "HTU_reduceSUM_mapGRU", "concat_mode": "tree", "use_tfidf": true, "leaf_fc_size": 0, "learning_rate": 0.0003, "batch_size": 100}
