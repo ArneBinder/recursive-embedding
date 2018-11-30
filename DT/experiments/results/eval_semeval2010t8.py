c3101032f58da8f8cb39f8ab14ab024d79afd265
Arne Binder <arne.b.binder@gmail.com>
2018-11-30 18:28:17 +0100

    improved eval script

--- DT/experiments/results/eval_semeval2010t8.py
+++ DT/experiments/results/eval_semeval2010t8.py
@@ -3,10 +3,21 @@ import subprocess
 import numpy as np
 
 
+def format_rel(rel):
+    rel_split = rel.strip().split('/')
+    if rel_split[-1] == 'Other':
+        return rel_split[-1]
+    if rel_split[-2] == 'FW':
+        return '%s(e1,e2)' % rel_split[-1]
+    if rel_split[-2] == 'BW':
+        return '%s(e2,e1)' % rel_split[-1]
+    raise Exception('unknown format for relation: %s' % rel)
+
+
 def ids_to_str(sc, fn_in, fn_out):
     v = np.load(fn_in)
     open(fn_out, 'w').writelines(
-        ('%i\t%s\n' % (i, sc[idx].split('/')[-1]) for i, idx in enumerate(np.argmax(v, axis=1))))
+        ('%i\t%s\n' % (i, format_rel(sc[idx])) for i, idx in enumerate(np.argmax(v, axis=1))))
 
 
 def convert_values(path, fn_class_strings):
@@ -20,9 +31,20 @@ def convert_values(path, fn_class_strings):
 
 def eval(path_dir, fn_script='~/recursive-embedding/docker/create-corpus/semeval2010task8/data/SemEval2010_task8_scorer-v1.2/semeval2010_task8_scorer-v1.2.pl',
          fn_class_strings='data.RELATION.classes.strings'):
-    fn_predicted_tsv, fn_gold_tsv = convert_values(path=path_dir, fn_class_strings=fn_class_strings)
+
+    fn_gold_strings = os.path.join(path_dir, 'values_gold_strings.txt')
+    fn_max_strings = os.path.join(path_dir, 'values_max_strings.txt')
+    if os.path.exists(fn_gold_strings) and os.path.exists(fn_max_strings):
+        fn_gold_tsv = os.path.join(path_dir, 'values_gold_strings.tsv')
+        fn_predicted_tsv = os.path.join(path_dir, 'values_max_strings.tsv')
+        open(fn_gold_tsv, 'w').writelines(('%i\t%s\n' % (i, format_rel(l)) for i, l in enumerate(open(fn_gold_strings).readlines())))
+        open(fn_predicted_tsv, 'w').writelines(('%i\t%s\n' % (i, format_rel(l)) for i, l in enumerate(open(fn_max_strings).readlines())))
+    else:
+        fn_predicted_tsv, fn_gold_tsv = convert_values(path=path_dir, fn_class_strings=fn_class_strings)
+
     check_script = 'perl %s %s %s' % (fn_script, fn_predicted_tsv, fn_gold_tsv)
     perl_result = subprocess.check_output(check_script, shell=True)
+    open(os.path.join(path_dir, 'eval.txt'), 'w').write(perl_result)
     last_line = perl_result.split('\n')[-2]
     score_str = last_line.replace('<<< The official score is (9+1)-way evaluation with directionality taken into account: macro-averaged F1 = ', '').replace('% >>>', '')
     f1 = float(score_str) / 100
