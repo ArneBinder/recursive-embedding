ca05bbbf476d6477d71ab9cf94f1e46ae8877691
Arne Binder <arne.b.binder@gmail.com>
2019-01-14 21:52:42 +0100

    adapt file paths

--- convert_corpora.sh
+++ convert_corpora.sh
@@ -9,12 +9,13 @@ CORPORA_OUT="/mnt/DATA/ML/data/corpora_out"
 GLOVE_TXT="/mnt/DATA2/NLP/corpora/glove.840B.300d.txt"
 
 ## convert SICK
-python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/SICK_RDF/spacy -c sck:vocab#entailment_judgment -g "$GLOVE_TXT" -m 2
-python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/SICK_RDF/corenlp -c sck:vocab#entailment_judgment -g "$GLOVE_TXT" -m 2
+python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/SICK_RDF/spacy_noner -c sck:vocab#entailment_judgment -g "$GLOVE_TXT" -m 2
+python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/SICK_RDF/corenlp_noner -c sck:vocab#entailment_judgment -g "$GLOVE_TXT" -m 2
 
+#TODO: execute!
 ## convert IMDB
-python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/IMDB_RDF/spacy_noner -c imdb:vocab#sentiment -g "$GLOVE_TXT" -m 2
-python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/IMDB_RDF/corenlp_noner -c imdb:vocab#sentiment -g "$GLOVE_TXT" -m 2
+#python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/IMDB_RDF/spacy_noner -c imdb:vocab#sentiment -g "$GLOVE_TXT" -m 2
+#python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/IMDB_RDF/corenlp_noner -c imdb:vocab#sentiment -g "$GLOVE_TXT" -m 2
 
 ## convert SEMEVAL
 # entity masking; re-linked entities
@@ -27,6 +28,7 @@ python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/SEMEVAL2010T8_RDF/corenlp -c 
 python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/SEMEVAL2010T8_RDF/spacy -c smvl:vocab#relation -g "$GLOVE_TXT" -m 2 -t -l -e
 python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/SEMEVAL2010T8_RDF/corenlp -c smvl:vocab#relation -g "$GLOVE_TXT" -m 2 -t -l -e
 
+#TODO: fix this!
 ## convert TACRED
 # entity masking; re-linked entities
 python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/TACRED_RDF/None -c tac:vocab#relation -g "$GLOVE_TXT" -m 2 -t -l
