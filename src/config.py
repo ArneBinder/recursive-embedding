9e2f2a8393861ed2edde97abd472b4ff71035a56
Arne Binder <arne.b.binder@gmail.com>
2019-01-16 19:38:24 +0100

    added parameters to parameter sets

--- src/config.py
+++ src/config.py
@@ -258,9 +258,9 @@ ALLOWED_TYPES = ['string', 'float', 'integer', 'boolean']
 TREE_MODEL_PARAMETERS = ['additional_vecs', 'leaf_fc_size', 'root_fc_sizes', 'state_size', 'tree_embedder']
 # used for compile trees
 TREE_STRUCTURE_PARAMETERS = ['max_depth', 'context', 'link_cost_ref', 'concat_mode', 'sequence_length',
-                             'no_fixed_vecs', 'all_vecs_fixed']
+                             'no_fixed_vecs', 'all_vecs_fixed', 'add_heads', 'blank', 'bidirectional']
 MODEL_PARAMETERS = TREE_MODEL_PARAMETERS + ['fc_sizes', 'use_tfidf', 'use_circular_correlation', 'model_type', 'task']
-DESCRIPTION_PARAMETERS = MODEL_PARAMETERS + TREE_STRUCTURE_PARAMETERS + ['dump_trees', 'var_vecs_zero', 'var_vecs_random']
+DESCRIPTION_PARAMETERS = MODEL_PARAMETERS + TREE_STRUCTURE_PARAMETERS + ['var_vecs_zero', 'var_vecs_random', 'sample_method']
 FLAGS_FN = 'flags.json'
 
 VALUES_SHORT = {'TRUE': 'T', 'FALSE': 'F'}
