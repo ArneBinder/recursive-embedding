a4c9ccd451c05b32195a6061e14ab1b9c6f6088a
Arne Binder <arne.b.binder@gmail.com>
2019-01-18 00:46:09 +0100

    dummy changes

--- docker/train/tensorflow-fold/train_search_re_tacred_direct_recnn.sh
+++ docker/train/tensorflow-fold/train_search_re_tacred_direct_recnn.sh
@@ -5,3 +5,4 @@ echo "USE_GPUS=$USE_GPUS"
 
 # TACRED relation extraction
 ./train.sh "$USE_GPUS" RE/TACRED/DIRECT/RECNN train-settings/general/gpu-search.env train-settings/model/recnn.env train-settings/task/re-tacred.env train-settings/dataset/corenlp/tacred-direct.env
+
--- docker/train/tensorflow-fold/train_search_re_tacred_edges_recnn.sh
+++ docker/train/tensorflow-fold/train_search_re_tacred_edges_recnn.sh
@@ -5,4 +5,3 @@ echo "USE_GPUS=$USE_GPUS"
 
 # TACRED relation extraction
 ./train.sh "$USE_GPUS" RE/TACRED/EDGES/RECNN train-settings/general/gpu-search.env train-settings/model/recnn.env train-settings/task/re-tacred.env train-settings/dataset/corenlp/tacred-edges.env
-
