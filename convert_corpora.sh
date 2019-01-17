b4c1037a5190d921fb65728c1fe6d88318d1a813
Arne Binder <arne.b.binder@gmail.com>
2019-01-17 15:34:19 +0100

    set min_count to 20 for IMDB and TACRED

--- convert_corpora.sh
+++ convert_corpora.sh
@@ -13,8 +13,8 @@ python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/SICK_RDF/spacy_noner -c sck:v
 python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/SICK_RDF/corenlp_noner -c sck:vocab#entailment_judgment -g "$GLOVE_TXT" -m 2
 
 ## convert IMDB
-python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/IMDB_RDF/spacy_noner -c imdb:vocab#sentiment -g "$GLOVE_TXT" -m 2
-python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/IMDB_RDF/corenlp_noner -c imdb:vocab#sentiment -g "$GLOVE_TXT" -m 2
+python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/IMDB_RDF/spacy_noner -c imdb:vocab#sentiment -g "$GLOVE_TXT" -m 20
+python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/IMDB_RDF/corenlp_noner -c imdb:vocab#sentiment -g "$GLOVE_TXT" -m 20
 
 ## convert SEMEVAL
 # entity masking; re-linked entities
@@ -29,8 +29,8 @@ python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/SEMEVAL2010T8_RDF/corenlp -c 
 
 ## convert TACRED
 # entity masking; re-linked entities
-python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/TACRED_RDF/None -c tac:vocab#relation -g "$GLOVE_TXT" -m 2 -t -l
+python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/TACRED_RDF/None -c tac:vocab#relation -g "$GLOVE_TXT" -m 20 -t -l
 # entity masking; entity-to-entity spans
-python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/TACRED_RDF/None -c tac:vocab#relation -g "$GLOVE_TXT" -m 2 -t -s
+python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/TACRED_RDF/None -c tac:vocab#relation -g "$GLOVE_TXT" -m 20 -t -s
 # entity masking; re-linked entities; link via deprel
-python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/TACRED_RDF/None -c tac:vocab#relation -g "$GLOVE_TXT" -m 2 -t -l -e
+python src/corpus_rdf.py CONVERT -i "$CORPORA_OUT"/TACRED_RDF/None -c tac:vocab#relation -g "$GLOVE_TXT" -m 20 -t -l -e
