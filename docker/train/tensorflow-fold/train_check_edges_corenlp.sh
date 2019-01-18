0dddbd48331c2c56e711bf0efab188f5ea699355
Arne Binder <arne.b.binder@gmail.com>
2019-01-18 18:04:01 +0100

    fixed check_data scripts

--- docker/train/tensorflow-fold/train_check_edges_corenlp.sh
+++ docker/train/tensorflow-fold/train_check_edges_corenlp.sh
@@ -16,16 +16,16 @@ echo "ENV_GENERAL=$ENV_GENERAL"
 ## RECNN
 
 # SICK relatedness
-./train.sh "$USE_GPUS" RELATEDNESS/SICK/DIRECT/RECNN train-settings/task/relatedness.env train-settings/model/recnn.env train-settings/dataset/corenlp/sick-edges.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" RELATEDNESS/SICK/EDGES/RECNN train-settings/task/relatedness.env train-settings/model/recnn.env train-settings/dataset/corenlp/sick-edges.env "$ENV_GENERAL"
 
 # SICK entailment
-./train.sh "$USE_GPUS" ENTAILMENT/SICK/DIRECT/RECNN train-settings/task/entailment.env train-settings/model/recnn.env train-settings/dataset/corenlp/sick-edges.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" ENTAILMENT/SICK/EDGES/RECNN train-settings/task/entailment.env train-settings/model/recnn.env train-settings/dataset/corenlp/sick-edges.env "$ENV_GENERAL"
 
 # SEMEVAL relation extraction
-./train.sh "$USE_GPUS" RE/SEMEVAL2010T8/DIRECT/RECNN train-settings/task/re-semeval.env train-settings/model/recnn.env train-settings/dataset/corenlp/semeval-edges.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" RE/SEMEVAL2010T8/EDGES/RECNN train-settings/task/re-semeval.env train-settings/model/recnn.env train-settings/dataset/corenlp/semeval-edges.env "$ENV_GENERAL"
 
 # TACRED relation extraction
-./train.sh "$USE_GPUS" RE/TACRED/DIRECT/RECNN train-settings/task/re-tacred.env train-settings/model/recnn.env train-settings/dataset/corenlp/tacred-edges.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" RE/TACRED/EDGES/RECNN train-settings/task/re-tacred.env train-settings/model/recnn.env train-settings/dataset/corenlp/tacred-edges.env "$ENV_GENERAL"
 
 # IMDB sentiment
-./train.sh "$USE_GPUS" SENTIMENT/IMDB/DIRECT/RECNN train-settings/task/sentiment.env train-settings/model/recnn.env train-settings/dataset/corenlp/imdb-edges.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" SENTIMENT/IMDB/EDGES/RECNN train-settings/task/sentiment.env train-settings/model/recnn.env train-settings/dataset/corenlp/imdb-edges.env "$ENV_GENERAL"
--- docker/train/tensorflow-fold/train_check_span_corenlp.sh
+++ docker/train/tensorflow-fold/train_check_span_corenlp.sh
@@ -15,19 +15,19 @@ echo "ENV_GENERAL=$ENV_GENERAL"
 ## RECNN
 
 # SEMEVAL relation extraction
-./train.sh "$USE_GPUS" RE/SEMEVAL2010T8/DIRECT/RECNN train-settings/task/re-semeval.env train-settings/model/recnn.env train-settings/dataset/corenlp/semeval-direct.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" RE/SEMEVAL2010T8/SPAN/RECNN train-settings/task/re-semeval.env train-settings/model/recnn.env train-settings/dataset/corenlp/semeval-span.env "$ENV_GENERAL"
 
 # TACRED relation extraction
-./train.sh "$USE_GPUS" RE/TACRED/DIRECT/RECNN train-settings/task/re-tacred.env train-settings/model/recnn.env train-settings/dataset/corenlp/tacred-direct.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" RE/TACRED/SPAN/RECNN train-settings/task/re-tacred.env train-settings/model/recnn.env train-settings/dataset/corenlp/tacred-span.env "$ENV_GENERAL"
 
 
 ## BOW
 
 # SEMEVAL relation extraction
-./train.sh "$USE_GPUS" RE/SEMEVAL2010T8/DIRECT/BOW train-settings/task/re-semeval.env train-settings/model/bow.env train-settings/dataset/corenlp/semeval-direct.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" RE/SEMEVAL2010T8/SPAN/BOW train-settings/task/re-semeval.env train-settings/model/bow.env train-settings/dataset/corenlp/semeval-span.env "$ENV_GENERAL"
 
 # TACRED relation extraction
-./train.sh "$USE_GPUS" RE/TACRED/DIRECT/BOW train-settings/task/re-tacred.env train-settings/model/bow.env train-settings/dataset/corenlp/tacred-direct.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" RE/TACRED/SPAN/BOW train-settings/task/re-tacred.env train-settings/model/bow.env train-settings/dataset/corenlp/tacred-span.env "$ENV_GENERAL"
 
 
 ## RNN -> not necessary, BOW creates flat data
