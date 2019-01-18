24f1dbd57b467ceae5fbc8b2a7aef87ff564d272
Arne Binder <arne.b.binder@gmail.com>
2019-01-18 18:34:55 +0100

    fixed train dev scripts

--- docker/train/tensorflow-fold/train_re_tacred_direct.sh
+++ docker/train/tensorflow-fold/train_re_tacred_direct.sh
@@ -4,7 +4,7 @@ USE_GPUS="$1"
 echo "USE_GPUS=$USE_GPUS"
 shift
 
-ENV_GENERAL="train-settings/dataset/corenlp/tacred-direct.env"
+ENV_GENERAL="train-settings/general/gpu-train-dev.env"
 ## use next argument as gpu nbr, if available
 if [ -n "$1" ]; then
     ENV_GENERAL="$1"
@@ -16,10 +16,10 @@ echo "ENV_GENERAL=$ENV_GENERAL"
 ## TACRED relation extraction on DIRECT data
 
 # RECNN
-./train.sh "$USE_GPUS" RE/TACRED/DIRECT/RECNN train-settings/general/gpu-train-dev.env train-settings/task/re-tacred.env train-settings/model/recnn.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" RE/TACRED/DIRECT/RECNN train-settings/task/re-tacred.env train-settings/model/recnn.env train-settings/dataset/corenlp/tacred-direct.env "$ENV_GENERAL"
 
 # BOW
-./train.sh "$USE_GPUS" RE/TACRED/DIRECT/RNN train-settings/general/gpu-train-dev.env train-settings/task/re-tacred.env train-settings/model/bow.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" RE/TACRED/DIRECT/RNN train-settings/task/re-tacred.env train-settings/model/bow.env train-settings/dataset/corenlp/tacred-direct.env "$ENV_GENERAL"
 
 # RNN
-./train.sh "$USE_GPUS" RE/TACRED/DIRECT/BOW train-settings/general/gpu-train-dev.env train-settings/task/re-tacred.env train-settings/model/rnn.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" RE/TACRED/DIRECT/BOW train-settings/task/re-tacred.env train-settings/model/rnn.env train-settings/dataset/corenlp/tacred-direct.env "$ENV_GENERAL"
--- docker/train/tensorflow-fold/train_re_tacred_span.sh
+++ docker/train/tensorflow-fold/train_re_tacred_span.sh
@@ -13,14 +13,13 @@ fi
 echo "ENV_GENERAL=$ENV_GENERAL"
 
 
-
 ## TACRED relation extraction on DIRECT data
 
 # RECNN
-./train.sh "$USE_GPUS" RE/TACRED/DIRECT/RECNN train-settings/task/re-tacred.env train-settings/model/recnn.env train-settings/dataset/corenlp/tacred-span.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" RE/TACRED/SPAN/RECNN train-settings/task/re-tacred.env train-settings/model/recnn.env train-settings/dataset/corenlp/tacred-span.env "$ENV_GENERAL"
 
 # BOW
-./train.sh "$USE_GPUS" RE/TACRED/DIRECT/RNN train-settings/task/re-tacred.env train-settings/model/bow.env train-settings/dataset/corenlp/tacred-span.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" RE/TACRED/SPAN/RNN train-settings/task/re-tacred.env train-settings/model/bow.env train-settings/dataset/corenlp/tacred-span.env "$ENV_GENERAL"
 
 # RNN
-./train.sh "$USE_GPUS" RE/TACRED/DIRECT/BOW train-settings/task/re-tacred.env train-settings/model/rnn.env train-settings/dataset/corenlp/tacred-span.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" RE/TACRED/SPAN/BOW train-settings/task/re-tacred.env train-settings/model/rnn.env train-settings/dataset/corenlp/tacred-span.env "$ENV_GENERAL"
