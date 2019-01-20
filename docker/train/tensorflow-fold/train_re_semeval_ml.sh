7e9a56f559fccde8c5fcba4825c1543e22d7748f
Arne Binder <arne.b.binder@gmail.com>
2019-01-20 01:29:16 +0100

    removed lm-re-smeveal/tacred env files (use re-semeval/tacred instead and overwrite entries)

--- docker/train/tensorflow-fold/train_re_semeval_ml.sh
+++ docker/train/tensorflow-fold/train_re_semeval_ml.sh
@@ -16,7 +16,7 @@ echo "ENV_GENERAL=$ENV_GENERAL"
 ## SEMEVAL2010T8 relation extraction via LM
 
 ## DIRECT + add_heads=EDG
-./train.sh "$USE_GPUS" RE/SEMEVAL2010T8/DIRECT/LM train-settings/task/lm-re-semeval.env train-settings/model/ml.env train-settings/dataset/corenlp/semeval-direct.env train-settings/specific/add-heads-EDG.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" RE/SEMEVAL2010T8/DIRECT/LM train-settings/task/re-semeval.env train-settings/model/ml.env train-settings/dataset/corenlp/semeval-direct.env train-settings/specific/add-heads-EDG.env "$ENV_GENERAL"
 
 ## EDGES
-./train.sh "$USE_GPUS" RE/SEMEVAL2010T8/EDGES/LM train-settings/task/lm-re-semeval.env train-settings/model/ml.env train-settings/dataset/corenlp/semeval-edges.env "$ENV_GENERAL"
+./train.sh "$USE_GPUS" RE/SEMEVAL2010T8/EDGES/LM train-settings/task/re-semeval.env train-settings/model/ml.env train-settings/dataset/corenlp/semeval-edges.env "$ENV_GENERAL"
