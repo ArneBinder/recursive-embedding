ad3b6bed80cc4e00175829f1faa4251e86129f30
Arne Binder <arne.b.binder@gmail.com>
2019-01-29 18:16:01 +0100

    added train scripts for re_semeval_direct/edges/span_recnn

---
+++ docker/train/tensorflow-fold/train_re_semeval_direct_recnn.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+USE_GPUS="$1"
+echo "USE_GPUS=$USE_GPUS"
+shift
+
+ENV_GENERAL="train-settings/general/gpu-train-dev.env"
+## use next argument as gpu nbr, if available
+if [ -n "$1" ]; then
+    ENV_GENERAL="$1"
+    shift
+fi
+echo "ENV_GENERAL=$ENV_GENERAL"
+
+
+## SEMEVAL2010T8 relation extraction
+
+## DIRECT
+
+# RECNN
+./train.sh "$USE_GPUS" RE/SEMEVAL2010T8/DIRECT/RECNN train-settings/task/re-semeval.env train-settings/model/recnn.env train-settings/dataset/corenlp/semeval-direct.env "$ENV_GENERAL"
