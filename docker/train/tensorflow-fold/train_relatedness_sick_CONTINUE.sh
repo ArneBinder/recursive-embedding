88301f65ef645aff17a9c886cf5cbf8835bd5d68
Arne Binder <arne.b.binder@gmail.com>
2019-03-06 17:05:56 +0100

    fixed logdir_continue script

--- docker/train/tensorflow-fold/train_relatedness_sick_CONTINUE.sh
+++ docker/train/tensorflow-fold/train_relatedness_sick_CONTINUE.sh
@@ -15,7 +15,7 @@ echo "ENV_GENERAL=$ENV_GENERAL"
 
 ## SICK relatedness prediction
 
-LOGDIR_CONTINUE=FINAL/RELATEDNESS/SICK/DIRECT/RECNN2/aEDG_avfF_bs100_dF_bPOS-NIFW-PAR_clp5.0_cmTREE_cntxt0_dfidx4-5_dtF_ec_fc_kp0.9_kpb1.0_kpn1.0_leaffc0_lr0.001_lc-1_dpth20_mtSIMTUPLE_n10000_ns20_nfvF_rootfc300_sm_sl50_st150_tk_dataCORENLPNONERRECEMBMC2_teHTUREDUCESUMMAPGRU_ccF_tfidfF_vvrF_vvzF/0.cp,
+LOGDIR_CONTINUE=RELATEDNESS/SICK/DIRECT/RECNN2/aEDG_avfF_bs100_dF_bPOS-NIFW-PAR_clp5.0_cmTREE_cntxt0_dfidx4-5_dtF_ec_fc_kp0.9_kpb1.0_kpn1.0_leaffc0_lr0.001_lc-1_dpth20_mtSIMTUPLE_n10000_ns20_nfvF_rootfc300_sm_sl50_st150_tk_dataCORENLPNONERRECEMBMC2_teHTUREDUCESUMMAPGRU_ccF_tfidfF_vvrF_vvzF/0.cp,
 export LOGDIR_CONTINUE
 
 ## CONTINUE
